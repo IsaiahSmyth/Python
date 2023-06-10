b014619c31f4a18ed0aaeade4f1a910988a5456f
Isaiah <smyth.isaiah@gmail.com>
2023-06-10 02:21:06 -0600

    First commit

---
+++ MathDojo.py
@@ -0,0 +1,24 @@
+# Create a MathDojo class
+class MathDojo:
+    def __init__(self):
+        self.result = self
+
+# Write the add method and test it by calling it 3 times, with different numbers of arguments each time
+
+
+def add(self, num, *num2):
+    pass
+
+
+MathDojo.add().add(2).add(2, 5, 1)
+# Write the subtract method and test it by calling it 3 times, with different numbers of arguments each time
+
+
+def subtract(self, num, *num2):
+    pass
+
+
+MathDojo.subtract().subtract(2).subtract(2, 5, 1)
+
+
+# Make sure you are able to chain methods as demonstrated above
