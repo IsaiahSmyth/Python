1ad5f5d53e342157cc3bd3f00eff5090f4c15bbe
Isaiah <smyth.isaiah@gmail.com>
2023-06-10 19:43:29 -0600

    First Commit

---
+++ Selection_sort.py
@@ -0,0 +1,10 @@
+# Write the algorithm for selection sort.
+
+def selection_sort(list):
+    for i in range(len(list)):
+        min_index = i
+        for j in range(i+1, len(list))
+        if list[j] < list[min_index]
+        min_index = j
+        list[i], list[min_index] = list[min_index], list[i]
+        return list
